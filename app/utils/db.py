8b3083dcc79de7e99255b0b1786a068073025799
egi <egigjevori15@gmail.com>
2025-03-18 16:47:36 +0100

    feat(database): integrate async database connection
    Add async database connection management using asyncpg and dotenv. Initialize database with a table for waste entries. Update FastAPI app to use a custom lifespan for managing database connections. Add .env to .gitignore and include python-dotenv in dependencies

---
+++ app/utils/db.py
@@ -0,0 +1,37 @@
+import os
+
+import asyncpg
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DATABASE_URL = (f"postgresql://{os.getenv('POSTGRES_USER')}"
+                f":{os.getenv('POSTGRES_PASSWORD')}"
+                f"@{os.getenv('POSTGRES_HOST')}"
+                f"/{os.getenv('POSTGRES_DB')}")
+
+pool: asyncpg.Pool
+
+async def connect():
+    global pool
+    pool = await asyncpg.create_pool(DATABASE_URL)
+
+async def disconnect():
+    await pool.close()
+
+async def initdb():
+    async with pool.acquire() as conn:
+        conn: asyncpg.Connection = conn
+        await conn.execute("""
+create table if not exists waste_entries
+(
+    id        serial
+        primary key,
+    type      varchar(100)     not null,
+    weight    double precision not null,
+    timestamp timestamp        not null,
+    user_id   integer          not null,
+    team_id   integer          not null
+);
+        """)
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.utils import db
 
+
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    await db.connect()
+    await db.initdb()
+    yield
+    await db.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
